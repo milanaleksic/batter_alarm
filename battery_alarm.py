ed944769375ab1c9bf0fd985dd4aa820f4ab87b9
Milan Aleksic <Milan.Aleksic@tomtom.com>
2019-05-28 10:38:35 +0200

    first commit

---
+++ battery_alarm.py
@@ -0,0 +1,30 @@
+import subprocess
+
+THRESHOLD = 97
+
+def run():
+    output = subprocess.check_output(["acpi"], universal_newlines=True)
+    for s in output.split("\n"):
+        s = s.strip()
+        if len(s) == 0:
+            continue
+        perc = int(s.split(" ")[3].split('%')[0])
+        try:
+            if perc < THRESHOLD and s.split(" ")[2].find("Discharging") != -1:
+                subprocess.call(["notify-send",
+                                 "--icon",
+                                "/usr/share/icons/gnome/32x32/status/battery-caution.png",
+                                 "-u",
+                                 "critical", "Battery at %d" % perc
+                                 ])
+        except Exception as e:
+            subprocess.call(["notify-send",
+                             "--icon",
+                             "/usr/share/icons/gnome/32x32/status/battery-caution.png",
+                             "-u",
+                             "critical", "Failed to run:" + e.__str__()
+                             ])
+
+
+if __name__ == '__main__':
+    run()
